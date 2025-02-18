3eb2e6e026393cd1d2c29ddf629e50aaad7cb7c7
cuongpmph45042 <cuongpmph45042@fpt.edu.vn>
2025-02-18 11:35:24 +0700

    chi tiết

--- resources/views/client/product/index.blade.php
+++ resources/views/client/product/index.blade.php
@@ -5,27 +5,6 @@ Chi tiết sản phẩm
 @endsection
 
 @section('content')
-<!--== Start Page Header Area Wrapper ==-->
-<div class="page-header-area" data-bg-img="assets/img/photos/bg3.webp">
-    <div class="container pt--0 pb--0">
-        <div class="row">
-            <div class="col-12">
-                <div class="page-header-content">
-                    <h2 class="title" data-aos="fade-down" data-aos-duration="1000">Product Details</h2>
-                    <nav class="breadcrumb-area" data-aos="fade-down" data-aos-duration="1200">
-                        <ul class="breadcrumb">
-                            <li><a href="index.html">Home</a></li>
-                            <li class="breadcrumb-sep">//</li>
-                            <li>Product Details</li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!--== End Page Header Area Wrapper ==-->
-
 <!--== Start Product Single Area Wrapper ==-->
 <section class="product-area product-single-area">
     <div class="container">
