39faa6c5c24759c0c6e77e5ecc524f837820edf0
cuongpmph45042 <cuongpmph45042@fpt.edu.vn>
2025-02-18 22:59:28 +0700

    giao diện admin

---
+++ resources/views/admin/layout/partials/css.blade.php
@@ -0,0 +1,9 @@
+<!-- Libraries Stylesheet -->
+<link href="{{ asset('administrator/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+<link href="{{ asset('administrator/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css')}}" rel="stylesheet" />
+
+<!-- Customized Bootstrap Stylesheet -->
+<link href="{{asset('administrator/css/bootstrap.min.css')}}" rel="stylesheet">
+
+<!-- Template Stylesheet -->
+<link href="{{asset('administrator/css/style.css')}}" rel="stylesheet">
