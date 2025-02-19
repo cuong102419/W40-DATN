9caa2da69cd82c136c67703f8be09c7f53ef73b7
unknown <hoang021104@gmail.com>
2025-02-19 14:38:56 +0700

    ud

--- resources/views/admin/layout/partials/nav.blade.php
+++ resources/views/admin/layout/partials/nav.blade.php
@@ -79,12 +79,13 @@
             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                 <img class="rounded-circle me-lg-2" src="{{ asset('administrator/img/user.jpg')}}" alt=""
                     style="width: 40px; height: 40px;">
-                <span class="d-none d-lg-inline-flex">John Doe</span>
+                <span class="d-none d-lg-inline-flex">Acount</span>
             </a>
             <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                 <a href="#" class="dropdown-item">My Profile</a>
                 <a href="#" class="dropdown-item">Settings</a>
                 <a href="#" class="dropdown-item">Log Out</a>
+                <a href="{{route('home')}}" class="dropdown-item">Return to client page</a>
             </div>
         </div>
     </div>
