832c28b4573994d9945957d15a466287768bfba8
cuongpmph45042 <cuongpmph45042@fpt.edu.vn>
2025-03-03 23:42:40 +0700

    sửa ảnh profile

--- resources/views/client/user/index.blade.php
+++ resources/views/client/user/index.blade.php
@@ -14,7 +14,7 @@
                 </tr>
                 <tr>
                     <td colspan="2" class="text-center">
-                        <img width="130" class="rounded" src="{{ Storage::url(Auth::user()->avatar) ??  asset('administrator/img/tho_7.png') }}" alt="">
+                        <img width="130" class="rounded" src="{{ Auth::user()->avatar ? Storage::url(Auth::user()->avatar) :  asset('administrator/img/tho_7.png') }}" alt="">
                     </td>
                 </tr>
                 <tr>
