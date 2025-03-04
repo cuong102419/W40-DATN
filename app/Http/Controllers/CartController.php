ee044fb0960a4b6572e1c893798f7824d94f5019
nthoang021104 <hoang021104@gmail.com>
2025-03-04 21:52:16 +0700

    delete cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -11,37 +11,5 @@ use Darryldecode\Cart\Facades\CartFacade as Cart;
 
 class CartController extends Controller
 {
-    public function index() {
-
-        $cart = ModelsCart::getContent();
-        // dd($cart);
-         // Lấy toàn bộ sản phẩm trong giỏ hàng
-        return view('client.cart.index', compact('cart'));
-    
-    }
-    
-
-    // Thêm sản phẩm vào giỏ hàng
-    public function addToCart(Request $request)
-{
-    // dd($request->all());
-    
-
-    $cart = session()->get('cart', []);
-
-    // Thêm sản phẩm vào giỏ hàng
-    $cart[$request->id] = [
-        "name" => $request->name,
-        "price" => $request->price ?? 0,
-        "quantity" => ($cart[$request->id]['quantity'] ?? 0) + 1,
-        "image" => $request->image
-    ];
-
-    session()->put('cart', $cart);
-    // dd($cart);
-    // 🔥 TRẢ VỀ GIAO DIỆN
-
-    return view('client.cart.index', compact('cart'));
-}
-
+    //
 }
