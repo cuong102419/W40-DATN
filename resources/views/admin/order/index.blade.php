2c3e2977d429dc6eae0ff1e65e92f6e1ba619518
sonduan1 <sonttph45016@fpt.edu.vn>
2025-04-01 23:48:31 +0700

    list order, change detail order

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -11,28 +11,36 @@
                 <h6 class="mb-4">Danh sách đơn hàng</h6>
                 <div class="bg-white p-3">
                     <div class="table-responsive">
-                        <table class="table table-striped text-center">
+                        <table class="table table-striped text-center table-bordered">
                             <thead>
                                 <tr>
-                                    <th class="text-nowrap" style="width:1px">Mã đơn hàng</th>
+                                    <th>Mã đơn hàng</th>
+                                    <th>Khách hàng</th>
                                     <th>Thanh toán</th>
                                     <th>Trạng thái</th>
                                     <th>Tổng tiền</th>
                                     <th>Ngày tạo</th>
-                                    <th class="text-nowrap" style="width:1px">Hành động</th>
+                                    <th>Hành động</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach ($orders as $order)
+                                @foreach ($orders as $index => $order)
                                     <tr>
-                                        <td>{{ $order->id }}</td>
+                                        <th>{{ $order->order_code }}</th>
+                                        <td class="text-start text-nowrap" style="width:1px">
+                                            <ul>
+                                                <li>{{ $order->fullname }}</li>
+                                                <li>{{ $order->phone_number }}</li>
+                                                <li>{{ $order->email }}</li>
+                                            </ul>
+                                        </td>
                                         <td>
                                             <div>
                                                 <span>{{ $payment_method[$order->payment_method] }}</span>
                                             </div>
                                             <div>
-                                                <span
-                                                    class="{{$payment_status[$order->payment_status]['class']}} fw-bold">{{ $payment_status[$order->payment_status]['value']}}</span>
+                                                <small
+                                                    class="{{$payment_status[$order->payment_status]['class']}} fw-bold">{{ $payment_status[$order->payment_status]['value']}}</small>
                                             </div>
                                         </td>
                                         <td>
@@ -43,10 +51,10 @@
                                             <span
                                                 class="text-dark fw-bold">{{ number_format($order->total, 0, '.', '.') }}đ</span>
                                         </td>
-                                        <td>
+                                        <td class="text-start">
                                             {{ $order->created_at->format('d \t\h\g m, Y') }}
                                         </td>
-                                        <td class="text-center">
+                                        <td class="text-nowrap" style="width:1px">
                                             <a href="{{ route('admin-order.detail', $order->id) }}" class="btn text-primary"><i
                                                     class="fas fa-pen"></i></a>
                                         </td>
--- resources/views/client/cart/index.blade.php
+++ resources/views/client/cart/index.blade.php
@@ -65,10 +65,6 @@
                                                         class="price text-danger"><strong>{{ number_format($cart['price']) }}đ</strong></span>
                                                 </td>
                                                 <td class="product-quantity">
-                                                    {{-- <div class="pro-qty">
-                                                        <input type="text" class="quantity" name="quantity[{{ $cart['id'] }}]"
-                                                            title="Quantity" value="{{ $cart['quantity'] }}">
-                                                    </div> --}}
                                                     <div class="pro-qty">
                                                         <div class="d-flex">
                                                             <div class= "dec qty-btn d-flex align-items-center justify-content-center">-</div>
