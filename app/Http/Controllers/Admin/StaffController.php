fc47dac017a2e6197b85f0bed4d79664603bb97f
TranDuyManh04 <manhtdph45017@fpt.edu.vn>
2025-04-14 20:35:15 +0700

    filter staff

--- app/Http/Controllers/Admin/StaffController.php
+++ app/Http/Controllers/Admin/StaffController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Staff;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class StaffController extends Controller
 {
-    public function index() {
-        $staffs = User::where('role', 'staff')->get();   
+    public function index(Request $request)
+    {
 
+        $query = User::where('role', 'staff');
+
+        if ($request->filled('date')) {
+            $query->whereDate('created_at', $request->date);
+        }
+
+        $staffs = $query->latest('id')->paginate(10);
+
+        $allStaffs = User::where('role', 'staff')->get();
         $status = [
             1 => ['value' => 'Hoạt động', 'class' => 'text-success'],
             0 => ['value' => 'Vô hiệu hóa', 'class' => 'text-danger']
@@ -18,20 +28,20 @@ class StaffController extends Controller
 
         return view('admin.staff.index', compact('staffs', 'status'));
     }
-    
-    public function status(Request $request, User $user) {
+
+    public function status(Request $request, User $user)
+    {
         $action = $request->input('action');
-        if($action == 'ban') {
+        if ($action == 'ban') {
             $user->status = 0;
             $user->save();
         }
 
-        if ($action == 'unban') { 
+        if ($action == 'unban') {
             $user->status = 1;
             $user->save();
         }
 
         return redirect()->back()->with('success', 'Cập nhật trạng thái tài khoản thành công.');
-
     }
 }
--- resources/views/admin/staff/index.blade.php
+++ resources/views/admin/staff/index.blade.php
@@ -14,55 +14,76 @@
                         </a>
                     </div>
                 </div>
-                <div class=" table-responsive bg-white ps-3 pe-3">
-                    <table class="table mt-4 table-striped">
-                        <thead>
-                            <tr class="text-center">
-                                <th>STT</th>
-                                <th>Họ tên</th>
-                                <th>EMAIL</th>
-                                <th>Chức năng</th>
-                                <th>Trạng thái</th>
-                                <th>Ngày tạo</th>
-                                <th>Hành động</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($staffs as $index => $staff)
+
+                <div>
+                    <div class="col-md-4">
+                        <form method="GET" action="{{ route('admin-staff.index') }}">
+                            <div class="d-flex align-items-center">
+                                <div class="me-2">
+                                    <label for="date">Chọn ngày:</label>
+                                    <input type="date" class="form-control form-control-sm" name="date" id="date"
+                                        value="{{ request('date') }}">
+                                </div>
+                                <div>
+                                    <button class="btn btn-sm btn-primary mt-3"><i class="fas fa-filter"></i> Lọc</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class=" table-responsive bg-white ps-3 pe-3">
+                        <table class="table mt-4 table-striped">
+                            <thead>
                                 <tr class="text-center">
-                                    <th>{{ $index + 1 }}</th>
-                                    <td>{{ $staff->name }}</td>
-                                    <td>{{ $staff->email }}</td>
-                                    <td>
-                                        <span>Nhân viên</span>
-                                    </td>
-                                    <td>
-                                        <span
-                                            class=" fw-bold {{ $status[$staff->status]['class'] }}">{{ $status[$staff->status]['value'] }}</span>
-                                    </td>
-                                    <td>
-                                        {{ $staff->created_at->format('d \T\h\á\n\g m, Y') }}
-                                    </td>
-                                    <td>
-                                        <form action="{{ route('admin-staff.status', $staff->id) }}" method="post">
-                                            @csrf
-                                            @method('PATCH')
-                                            @if ($staff->status == 1)
-                                                <button class="btn text-danger" name="action" value="ban"
-                                                    type="submit" onclick="return confirm('Bạn có muốn khóa tài khoản nây.')"><i class="fas fa-ban"></i></button>
-                                            @else
-                                                <button class="btn text-primary" name="action" value="unban"
-                                                    type="submit" onclick="return confirm('Bạn có muốn mo khoa tài khoản nây.')"><i class="fas fa-unlock-alt"></i></button>
-                                            @endif
-                                        </form>
-                                    </td>
+                                    <th>STT</th>
+                                    <th>Họ tên</th>
+                                    <th>EMAIL</th>
+                                    <th>Chức năng</th>
+                                    <th>Trạng thái</th>
+                                    <th>Ngày tạo</th>
+                                    <th>Hành động</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    {{-- {{ $staffs->links() }} --}}
+                            </thead>
+                            <tbody>
+                                @foreach ($staffs as $index => $staff)
+                                    <tr class="text-center">
+                                        <th>{{ $index + 1 }}</th>
+                                        <td>{{ $staff->name }}</td>
+                                        <td>{{ $staff->email }}</td>
+                                        <td>
+                                            <span>Nhân viên</span>
+                                        </td>
+                                        <td>
+                                            <span
+                                                class=" fw-bold {{ $status[$staff->status]['class'] }}">{{ $status[$staff->status]['value'] }}</span>
+                                        </td>
+                                        <td>
+                                            {{ $staff->created_at->format('d \T\h\á\n\g m, Y') }}
+                                        </td>
+                                        <td>
+                                            <form action="{{ route('admin-staff.status', $staff->id) }}" method="post">
+                                                @csrf
+                                                @method('PATCH')
+                                                @if ($staff->status == 1)
+                                                    <button class="btn text-danger" name="action" value="ban"
+                                                        type="submit"
+                                                        onclick="return confirm('Bạn có muốn khóa tài khoản nây.')"><i
+                                                            class="fas fa-ban"></i></button>
+                                                @else
+                                                    <button class="btn text-primary" name="action" value="unban"
+                                                        type="submit"
+                                                        onclick="return confirm('Bạn có muốn mo khoa tài khoản nây.')"><i
+                                                            class="fas fa-unlock-alt"></i></button>
+                                                @endif
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        {{-- {{ $staffs->links() }} --}} 
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
